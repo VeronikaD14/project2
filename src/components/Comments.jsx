26c37781c8771ed13b21e9b1654958dca0625f2d
VeronikaD14 <3smile4me@gmail.com>
2023-06-02 11:00:19 +0100

    6 fit mobile version

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -2,17 +2,19 @@ function Comments ({comments}) {
     return (
         <>
         <h2>Comments</h2>
-        {comments.map(comment => {
-                return (
-                  <section className="borderElem">
-                    <h2>Name: {comment.author}</h2><br />
-                    <h2 >{comment.body}</h2>
-                   
-                    </section>
-                    )
-              })}
+        {comments.length > 0 ? (
+  comments.map((comment) => (
+    <section key={comment.id} className="borderElem">
+      <h2>Name: {comment.author}</h2>
+      <h2>{comment.body}</h2>
+    </section>
+  ))
+) : (
+  <h3>No comments</h3>
+)}
         </>
     )
 }
+
 
 export default Comments
