0d28bd7575eb7f07987fde67803b77ce98658345
VeronikaD14 <3smile4me@gmail.com>
2023-06-02 16:11:51 +0100

    HomePage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,18 @@
 function Home () {
     return (
-        <>
-        <h2>Home Page</h2>
-        </>
+        <article>
+        <h2>Hello my Friend, its my first Project - Website. 
+            Created using React in Visual Studio Code.
+            Here we have:<br></br>
+            BackEnd: DataBase functionality created by me<br></br>
+            FrontEnd: its what you see now </h2>
+            <h3>Please fell free to visit Reviews Page<br></br>
+            where you can leave a comment, your user experience<br></br>
+            or if you can try to find a bag or some improvements<br></br>
+            I will be happy to know</h3><br></br>
+            <img src="https://img.uxwing.com/wp-content/themes/uxwing/download/emoji-emoticon/smile-icon.png"></img>
+            
+        </article>
     )
 }
 
